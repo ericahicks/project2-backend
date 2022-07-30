f6f21ead3b18b87ac8e4917658ec2b75633341ad
Erica Hicks <48265629+ericahicks@users.noreply.github.com>
2022-07-30 18:40:41 -0400

    first commit

---
+++ src/main/java/com/skillstorm/hotel/Project2HotelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2HotelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2HotelBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillstorm/hotel/Project2HotelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2HotelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
